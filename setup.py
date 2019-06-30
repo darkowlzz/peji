c2021b9a7598df3a2eb7660da466ef30d2281754
Sunny <me@darkowlzz.space>
2019-06-30 22:40:28 +0530

    add py_modules

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ from setuptools import setup, find_packages
 setup(
     name='peji',
     version='0.1.0',
-    # py_modules=['peji', 'peji.buttons', 'peji.page'],
-    packages=find_packages(),
+    py_modules=['peji', 'peji.page', 'peji.page.schemas'],
+    #packages=find_packages(),
     install_requires=[
         'Click',
         'bs4',
