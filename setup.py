61c973debb6bb1f4077c53660223d1c502c3a69c
Sunny <me@darkowlzz.space>
2019-09-23 02:03:04 +0530

    Set version to 0.0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='peji',
-    version='0.1.0',
+    version='0.0.4',
     # py_modules=['peji', 'peji.buttons', 'peji.page'],
     packages=find_packages(),
     install_requires=[
